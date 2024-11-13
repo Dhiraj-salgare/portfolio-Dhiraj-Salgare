a86ecc25d7db51ce1ffe7e4dda621d7c6feb73c6
Dhiraj-salgare <dhirajsalgare7057@gmail.com>
2024-11-13 22:53:09 +0530

    hello

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var tablinks=document.getElementsByClassName("tabs-links")
 var tabcontents=document.getElementsByClassName("tab-contents") 
+var menu=document.getElementsByClassName("fa-solid fa-bars")
+var mn=document.getElementsByClassName("hh")
 
 function opentab(tabname){
     for(tlink of tablinks){
@@ -12,4 +14,13 @@ function opentab(tabname){
 
     event.currentTarget.classList.add("active-links")
     document.getElementById(tabname).classList.add("active-tabs")
+}
+
+
+function jj(x){
+    x.classList.toggle("fa-xmark")
+   mn[0].classList.toggle("active");
+ 
+
+
 }
